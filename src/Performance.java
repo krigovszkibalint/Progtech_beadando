6e5697c9ae6341c3407e4d38748a91431e26c07c
Krigovszki Bálint <krigovszki.balint@gmail.com>
2022-05-07 18:59:35 +0200

    Decorated Class

---
+++ src/Performance.java
@@ -0,0 +1,21 @@
+public class Performance extends CarDecorator {
+
+    @Override
+    public String GetMake() {
+        return super.GetMake();
+    }
+
+    @Override
+    public String GetModel() {
+        return super.GetModel()+" Performance";
+    }
+
+    @Override
+    public int GetHorsePower() {
+        return 900;
+    }
+
+    public Performance(Car car) {
+        super(car);
+    }
+}
