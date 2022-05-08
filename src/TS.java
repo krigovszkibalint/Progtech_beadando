d1c87e97c7c25175090614449396d4f3ca4d69d8
Krigovszki Bálint <krigovszki.balint@gmail.com>
2022-05-08 15:58:01 +0200

    Decorator kibővítése új modellel

---
+++ src/TS.java
@@ -0,0 +1,26 @@
+public class TS extends CarDecorator {
+
+    @Override
+    public String GetMake() {
+        return super.GetMake();
+    }
+
+    @Override
+    public String GetModel() {
+        return super.GetModel()+" T Sport";
+    }
+
+    @Override
+    public int GetSeats() {
+        return 4;
+    }
+
+    @Override
+    public int GetHorsePower() {
+        return 192;
+    }
+
+    public TS(Car car) {
+        super(car);
+    }
+}
